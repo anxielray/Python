3b5bd8bc2a2037f6dc082a5cf328d0db1384a2ee
anxielray <anxielworld@gmail.com>
2025-02-14 04:49:53 +0300

    [Input]:Add script for questioning the user foe input, converting string to int and printing the input to the console

---
+++ Quest1/credentials.py
@@ -0,0 +1,7 @@
+#write a peogram that will ask for the name and the age feom the user
+# and print them to the console
+
+
+name = input("What is your name?")
+age = int(input("ehat's your age"))
+print(f"Your name is {name} and you are {age} years old")
