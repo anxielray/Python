818e853d13cf4a81f9e69d6bcef2c921cfe25504
anxielray <anxielworld@gmail.com>
2025-02-14 09:18:54 +0300

    [Quest 1]: Add factorial for a number in Python

---
+++ Quest1/factorial.py
@@ -0,0 +1,22 @@
+# write a function displaying thr factorial of n
+
+def factorial(n):
+    if n < 0:
+        return None
+    elif n == 0:
+        return 1
+    else:
+        result = 1
+        for i in range(1, n+1):
+            result *= i
+        return result
+
+
+if __name__ == "__main__":
+    num = factorial(int(input("insert a number ")))
+
+    if num is not None:
+        print(f"factorial is {num}")
+    else:
+        print("No factorial us defined for negative Numbers")
+
