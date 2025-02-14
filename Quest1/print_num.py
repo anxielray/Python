72bab58b0f88930b87bd612918081c4ba77572a1
anxielray <anxielworld@gmail.com>
2025-02-14 08:45:45 +0300

    [Module]: Add initial commit woth an import

--- Quest1/print_num.py
+++ Quest1/print_num.py
@@ -1,18 +1,27 @@
 # Write a program that pick  a number from the user and prints it back.
+import sys
 
-def get_user_credentials():
+def define_even_odd():
     while True:
         try:
         
-            user_number = input('Enter a number')
+            user_number = input('Enter a number ')
+
+            if user_number== "exit":
+                sys.exit()
             num = float(user_number)
             return num
         except ValueError:
             print("Invalid input for a number")
 
 if __name__ == "__main__" :
-    unum = get_user_credentials()
+    unum = define_even_odd()
+
     if unum is not None:
-        print(f"Your number is {unum}")
+        if unum%2 == 0:
+
+            print("Your number is Even")
+        else:
+            print("Your number is Odd")
     else:
         print("Enter a valid number")
