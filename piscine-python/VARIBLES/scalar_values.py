cf6d13ed147f97f0024df39ad37dd503218a0715
anxielray <anxielworld@gmail.com>
2025-04-14 15:38:34 -0400

    Add scalar values to scalar_values.py containing  the solution to the exercise of the first exercise of the first quest Variables

---
+++ piscine-python/VARIBLES/scalar_values.py
@@ -0,0 +1,15 @@
+# Define the following variables:
+#   - an integer called `epochs`
+#   - a float called `learning_rate`
+#   - a string called `model_name`
+#   - a boolean `use_gpu`
+# print each variable with its choice
+
+epochs = 7
+learning_rate = 0.1
+model_name = "sonnet"
+use_gpu = True
+print(f"epochs: {epochs}")
+print(f"learning_rate: {learning_rate}")
+print(f"model_name: {model_name}")
+print(f"use_gpu: {use_gpu}")
