90995b0afa28b368c0910eae08a2a88d46b82f9b
anxielray <anxielworld@gmail.com>
2025-04-14 16:49:24 -0400

    Add a file containng the functionalities of finding the mean, the minimum,  the maximum and the range ina data set.

---
+++ piscine-python/VARIBLES/name_me.py
@@ -0,0 +1,10 @@
+data = [3.2, 7.1, 4.6, 5.5, 9.0]
+
+min_vl = min(data)
+mx_vl = max(data)
+mean = sum(data) / len(data)
+r = mx_vl - min_vl
+print("Minimum: ", min_vl)
+print("Maximum: ", mx_vl)
+print("Mean: ", mean)
+print("Range: ", r)
