d11a10246bc0ea1c334f9495c189980bf0e4b877
anxielray <anxielworld@gmail.com>
2025-04-14 16:06:21 -0400

    Add a file containing the solution to the question to read user input from the terminal and display a message formated in the formated string in Python programming language.

---
+++ piscine-python/VARIBLES/data_input.py
@@ -0,0 +1,5 @@
+name = input("Please enter your name: ")
+age = int(input("Please enter your age: "))
+fav_lang = input("Please enter your favorite language: ")
+
+print(f"Hello {name}, you are {age} years old and your favorite language is {fav_lang}.")
